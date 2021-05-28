93e39b72a77fffeb06baa927bfba6454af1292e0
DaddyWarbucks <beau@pxlpotion.com>
2021-05-28 18:17:43 -0500

    Seed more records

--- src/seed.js
+++ src/seed.js
@@ -1,17 +1,21 @@
 const faker = require('faker');
 const app = require('./app');
 
+const userTotal = 5000;
+const postsTotal = 10000;
+const commentsTotal = 50000;
+
 const randomIndex = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
-const bios = new Array(99).fill(null).map(() => {
+const bios = new Array(userTotal - 1).fill(null).map(() => {
   return {
     _id: faker.random.uuid(),
     content: faker.lorem.words()
   };
 });
 
-const users = new Array(99).fill(null).map((_, index) => {
+const users = new Array(userTotal - 1).fill(null).map((_, index) => {
   return {
     _id: faker.random.uuid(),
     name: faker.name.findName(),
@@ -37,20 +41,20 @@ users.push({
   bio_id: bio._id
 });
 
-const posts = new Array(5000).fill(null).map(() => {
+const posts = new Array(postsTotal).fill(null).map(() => {
   return {
     _id: faker.random.uuid(),
     title: faker.lorem.words(),
-    user_id: users[randomIndex(0, 99)]._id
+    user_id: users[randomIndex(0, userTotal - 1)]._id
   };
 });
 
-const comments = new Array(10000).fill(null).map(() => {
+const comments = new Array(commentsTotal).fill(null).map(() => {
   return {
     _id: faker.random.uuid(),
     comment: faker.lorem.words(),
-    user_id: users[randomIndex(0, 99)]._id,
-    post_id: posts[randomIndex(0, 4999)]._id
+    user_id: users[randomIndex(0, userTotal - 1)]._id,
+    post_id: posts[randomIndex(0, postsTotal - 1)]._id
   };
 });
 
