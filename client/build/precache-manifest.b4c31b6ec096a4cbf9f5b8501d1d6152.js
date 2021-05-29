cb0c66cc676614120203889ab7b4ddfbf7d4c684
DaddyWarbucks <beau@pxlpotion.com>
2021-05-28 20:57:42 -0500

    Build

---
+++ client/build/precache-manifest.b4c31b6ec096a4cbf9f5b8501d1d6152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc41e423cb6f3fa857386238d812ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8452db4a528304a664f",
+    "url": "/static/js/2.dff8bb49.chunk.js"
+  },
+  {
+    "revision": "fa4f55b6ab572782c2c24240e5ecf3d8",
+    "url": "/static/js/2.dff8bb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab98368492cd4fffefb1",
+    "url": "/static/js/main.30fdb00a.chunk.js"
+  },
+  {
+    "revision": "4d1c20842390c5410214",
+    "url": "/static/js/runtime-main.b65287f7.js"
+  },
+  {
+    "revision": "1ac1a40b76a3f236b4f9f504da4cc404",
+    "url": "/static/media/yarn.1ac1a40b.lock"
+  }
+]);
